a5b7eb49985c643334cdc2aa14b0b2a28f0823e4
Pedro Souza <mypedrosouza@gmail.com>
2025-08-18 16:23:20 -0300

    project: removed central/nexus publication / organize the project

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -1,69 +1,70 @@
 plugins {
-    alias(libs.plugins.kotlinJvm)
-    alias(libs.plugins.kotlinPluginSerialization)
-    `java-library`
-    `maven-publish`
-    signing
+  alias(libs.plugins.kotlinJvm)
+  alias(libs.plugins.kotlinPluginSerialization)
+  `java-library`
+  `maven-publish`
 }
 
 val pluginVersion: String by project
 val kotlinVersion = libs.versions.kotlin.get()
 val velocityVersion = libs.versions.velocity.get()
 val snapshotQualifier =
-    project.findProperty("isSnapshot")?.toString()?.toBoolean()?.let { if (it) "-SNASPHOT" else "" } ?: ""
+  project.findProperty("isSnapshot")?.toString()?.toBoolean()?.let { if (it) "-SNAPSHOT" else "" }
+    ?: ""
 version = "$pluginVersion+kotlin.$kotlinVersion$snapshotQualifier"
 
 repositories {
-    mavenCentral()
+  mavenCentral()
 }
 
 dependencies {
-    compileOnlyApi(libs.velocityApi)
+  compileOnlyApi(libs.velocityApi)
+  annotationProcessor(libs.velocityApi)
 
-    api(libs.bundles.kotlinLibraries)
-    api(libs.adventureExtraKotlin)
-    api(libs.configurateExtraKotlin)
+  api(libs.bundles.kotlinLibraries)
+  api(libs.adventureExtraKotlin)
+  api(libs.configurateExtraKotlin)
 }
 
 publishing {
-    publications {
-        create<MavenPublication>(project.name) {
-            from(components["java"])
+  publications {
+    create<MavenPublication>(project.name) {
+      from(components["java"])
 
-            pom {
-                name.set("velocity-language-kotlin")
-                url.set("https://github.com/morapowered/velocity-language-kotlin")
+      group = "com.velocitypowered"
+      artifactId = "velocity-language-kotlin"
+      version = project.version.toString()
 
-                licenses {
-                    license {
-                        name.set("MIT License")
-                        url.set("https://www.opensource.org/licenses/mit-license.php")
-                    }
-                }
+      pom {
+        name.set("velocity-language-kotlin")
+        url.set("https://github.com/morapowered/velocity-language-kotlin")
 
-                developers {
-                    developer {
-                        id.set("velocitycontributors")
-                        name.set("Velocity Contributors")
-                    }
-
-                    developer {
-                        id.set("eupedroosouza")
-                        name.set("Pedro Souza")
-                        email.set("66704494+eupedroosouza@users.noreply.github.com")
-                    }
-                }
-
-                scm {
-                    connection.set("scm:git:git://github.com/morapowered/velocity-language-kotlin.git")
-                    developerConnection.set("scm:git:ssh://github.com/morapowered/velocity-language-kotlin.git")
-                    url.set("https://github.com/morapowered/velocity-language-kotlin")
-                }
-            }
+        licenses {
+          license {
+            name.set("MIT License")
+            url.set("https://www.opensource.org/licenses/mit-license.php")
+          }
         }
-    }
-}
-signing {
-    sign(publishing.publications[project.name])
-}
 
+        developers {
+          developer {
+            id.set("velocitycontributors")
+            name.set("Velocity Contributors")
+          }
+
+          developer {
+            id.set("eupedroosouza")
+            name.set("Pedro Souza")
+            email.set("66704494+eupedroosouza@users.noreply.github.com")
+          }
+        }
+
+        scm {
+          connection.set("scm:git:git://github.com/morapowered/velocity-language-kotlin.git")
+          developerConnection.set("scm:git:ssh://github.com/morapowered/velocity-language-kotlin.git")
+          url.set("https://github.com/morapowered/velocity-language-kotlin")
+        }
+      }
+    }
+  }
+}
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,39 +1,9 @@
-plugins {
-    alias(libs.plugins.nexusPublishPlugin)
-    signing
-}
-
-
-
 subprojects {
-    group = "io.github.morapowered"
+    group = "com.velocitypowered"
 
     repositories {
         mavenCentral()
         maven("https://repo.papermc.io/repository/maven-public/")
         mavenLocal()
-    }
-}
-
-
-nexusPublishing {
-    repositories {
-        sonatype {
-            nexusUrl.set(uri("https://ossrh-staging-api.central.sonatype.com/service/local/"))
-            snapshotRepositoryUrl.set(uri("https://central.sonatype.com/repository/maven-snapshots/"))
-            username.set(project.findProperty("sonatype.username")?.toString() ?: System.getenv("SONATYPE_USERNAME"))
-            password.set(project.findProperty("sonatype.password")?.toString() ?: System.getenv("SONATYPE_PASSWORD"));
-        }
-    }
-}
-
-signing {
-    val signedKey = project.findProperty("signed.key")?.toString() ?: System.getenv("GPG_SECRET_KEY")
-    val signedPassword = project.findProperty("signed.password")?.toString() ?: System.getenv("GPG_PASSPHRASE")
-
-    if (signedKey != null && signedPassword != null) {
-        useInMemoryPgpKeys(signedKey, signedPassword)
-    } else {
-        useGpgCmd()
     }
 }
